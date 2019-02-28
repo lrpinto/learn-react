5f16815220de19ff8c1d0a6e7e181cea650d41cb
Luisa Pinto <xtremelu@gmail.com>
2019-02-28 14:59:31 +0000

    Making an Interactive Component

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -2,14 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {this.props.value}
-        </button>
-      );
+  class Square extends React.Component {
+    /* Added a constructor to this class in order to initialise state */
+    constructor(props) {
+      super(props);
+      this.state = {
+        value: null
+      }; /** React components can have state by setting this.state in their constructors. 
+           * this.state should be considered as private to a React component that it’s defined in 
+           */
     }
+
+      render() {
+        return (
+          <button 
+            className="square" 
+            onClick={() => this.setState({value: 'X'})} /**
+                                                         * By calling this.setState from an onClick handler in the Square’s render method, 
+                                                         * we tell React to re-render that Square whenever its <button> is clicked 
+                                                         * 
+                                                         * Notice how with onClick={() => alert('click')}, 
+                                                         * we’re passing a function as the onClick prop. It only fires after a click. 
+                                                         * Forgetting () => and writing onClick={alert('click')} is a common mistake, 
+                                                         * and would fire the alert every time the component re-renders.
+                                                         */
+            >
+              {this.state.value}
+          </button>
+        );
+      }
   }
   
   class Board extends React.Component {
